7cca553062d3bae97b924809f7368b26889a3c4a
srsviegas <rudiero.viegas@gmail.com>
2023-08-26 12:57:12 -0300

    FEAT: Implement the remaining system states

--- script.js
+++ script.js
@@ -5,8 +5,19 @@ const execution = document.getElementById("execution");
 
 class System {    
     transitions = {
-        0: Object.fromEntries(destinations.map(d => [d, 1])),
-        1: {"Visto": 2, "Cidadania": 3, "Voltar": 0}
+        0: Object.fromEntries(destinations),
+        1: {"VISTO": 4, "CIDADANIA": 4},
+        4: {"SOMENTE IDA": 6, "IDA E VOLTA": 5, "VOLTAR": 0},
+        5: {"F": 10, "J": 10, "W": 10, "Y": 10, "VOLTAR": 4},
+        6: {"F": 7, "J": 7, "W": 7, "Y": 7, "VOLTAR": 4},
+        7: Object.fromEntries(months.map((m) => [m, 8]).concat([["VOLTAR", 6]])),
+        8: Object.fromEntries([...Array(31).keys()].map((m) => [m, 16])),
+        10: Object.fromEntries(months.map((m) => [m, 11]).concat([["VOLTAR", 5]])),
+        11: Object.fromEntries([...Array(31).keys()].map((m) => [m, 13])),
+        13: Object.fromEntries(months.map((m) => [m, 14]).concat([["VOLTAR", 10]])),
+        14: Object.fromEntries([...Array(31).keys()].map((m) => [m, 16])),
+        16: {"DÉBITO": 20, "CRÉDITO": 18},
+        17: Object.fromEntries([...Array(12).keys()].map(key => [(key + 1) + "X", 20]).concat([["VOLTAR", 16]]))
     };
 
     constructor() {
@@ -21,7 +32,7 @@ class System {
 
     nextState(key) {
         execution.innerHTML += `${key}<br>`;
-        this.changeState(this.transitions[this.state][key]);
+        this.changeState(this.transitions[this.state][key.toUpperCase()]);
     }
 
     resetState() {
@@ -31,6 +42,7 @@ class System {
 }
 
 let system = new System();
+console.log(system.transitions);
 
 function processInput() {
     let previousState = system.state

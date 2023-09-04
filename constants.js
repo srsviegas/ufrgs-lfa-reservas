c0db0ee1d4655ef4fb7f27ac806233830ef1110d
srsviegas <rudiero.viegas@gmail.com>
2023-09-04 17:39:30 -0300

    FEAT: Add date validation

--- constants.js
+++ constants.js
@@ -40,16 +40,16 @@ const destinations = [
 ];
 
 const months = [
-    "JANEIRO",
-    "FEVEREIRO",
-    "MARÇO",
-    "ABRIL",
-    "MAIO",
-    "JUNHO",
-    "JULHO",
-    "AGOSTO",
-    "SETEMBRO",
-    "OUTUBRO",
-    "NOVEMBRO",
-    "DEZEMBRO"
-]
+    ["JANEIRO", 31],
+    ["FEVEREIRO", 28],
+    ["MARÇO", 31],
+    ["ABRIL", 30],
+    ["MAIO", 31],
+    ["JUNHO", 30],
+    ["JULHO", 31],
+    ["AGOSTO", 31],
+    ["SETEMBRO", 30],
+    ["OUTUBRO", 31],
+    ["NOVEMBRO", 30],
+    ["DEZEMBRO", 31]
+];
--- script.js
+++ script.js
@@ -13,6 +13,16 @@ function capitalize(string) {
     return string.toLowerCase().replace(/(?:^|\s)\S(?=\S)|^\w$/g, x => x.toUpperCase());
 }
 
+/**
+ * Checks if the given day is within the valid range for the given month.
+ * @param {string} month - The name of the month to check.
+ * @param {number} day - The number of the day to check.
+ * @returns {boolean} True if the day is a valid input for the month, false otherwise.
+ */
+function isValidDay(month, day) {
+    return (day > 0) && (day <= Object.fromEntries(months)[month]);
+}
+
 class System {
     // Dictionary containing all of the system's possible transitions.
     transitions = {
@@ -21,12 +31,15 @@ class System {
         4: {"SOMENTE IDA": 6, "IDA E VOLTA": 5, "VOLTAR": 0},
         5: {"F": 10, "J": 10, "W": 10, "Y": 10, "VOLTAR": 4},
         6: {"F": 7, "J": 7, "W": 7, "Y": 7, "VOLTAR": 4},
-        7: Object.fromEntries(months.map((m) => [m, 8]).concat([["VOLTAR", 6]])),
-        8: Object.fromEntries([...Array(31).keys()].map((m) => [m + 1, 16])),
-        10: Object.fromEntries(months.map((m) => [m, 11]).concat([["VOLTAR", 5]])),
-        11: Object.fromEntries([...Array(31).keys()].map((m) => [m + 1, 13])),
-        13: Object.fromEntries(months.map((m) => [m, 14]).concat([["VOLTAR", 10]])),
-        14: Object.fromEntries([...Array(31).keys()].map((m) => [m + 1, 16])),
+        7: Object.fromEntries(months.map((m) => [m[0], 8]).concat([["VOLTAR", 6]])),
+        8: Object.fromEntries([...Array(31).keys()].map((m) => [m + 1, 9])),
+        9: {"DATA OK": 16},
+        10: Object.fromEntries(months.map((m) => [m[0], 11]).concat([["VOLTAR", 5]])),
+        11: Object.fromEntries([...Array(31).keys()].map((m) => [m + 1, 12])),
+        12: {"DATA OK": 13},
+        13: Object.fromEntries(months.map((m) => [m[0], 14]).concat([["VOLTAR", 10]])),
+        14: Object.fromEntries([...Array(31).keys()].map((m) => [m + 1, 15])),
+        15: {"DATA OK": 16},
         16: {"DÉBITO": 20, "CRÉDITO": 18},
         18: Object.fromEntries([...Array(12).keys()].map(key => [(key + 1) + "X", 20]).concat([["VOLTAR", 16]]))
     };
@@ -67,9 +80,21 @@ class System {
         if (system.state == undefined) {
             output = `<span class="error">${output} [ENTRADA INVÁLIDA]</span>`;
         }
+        if (key == "DATA OK") {
+            output = `<span class="success">${output}</span>`;
+        }
         this.currentExecution.innerHTML += `${output}<br>`;
         if (system.state == 20) {
             system.currentExecution.innerHTML += "<span class='success'>RESERVA FINALIZADA</span><br>";
+        }
+        else if ([9, 12, 15].includes(this.state)) {
+            const inputHistory = this.currentExecution.innerHTML.split("<br>").splice(-3);
+            if (isValidDay(inputHistory[0].toUpperCase(), Number(inputHistory[1]))) {
+                this.nextState("DATA OK");
+            }
+            else {
+                this.nextState("DATA INVÁLIDA");
+            }
         }
     }
 
